7b6f24fbba13ea71748da8725ea11a7aacdb978f
Subhajeet patnaik <118872450+Subhajeetgit@users.noreply.github.com>
2022-12-01 17:53:07 +0530

    Add files via upload
    Week taks

---
+++ week tasks.py
@@ -0,0 +1,57 @@
+a=int(input())
+b=int(input())
+c=int(input())
+print(a+b+c/3)
+
+
+n="Sonu"
+print(n[-1::-1])
+
+
+name="Subhajeet PaTnaIk"
+length=len(name)
+print(length)
+print(name.lower())
+print(name.lower())
+print(name.upper())
+print(name.title())
+print(name.count("e"))
+
+
+name=input()
+char=input()
+print(len(name))
+print(name.count(char))
+
+
+
+a="     Sonu    "
+dots="............................"
+print(a+dots)
+print(a.lstrip()+dots)
+print(a.rstrip()+dots)
+print(a.strip()+dots)
+
+
+a="You can easily crack thr placements"print(a.replace("You","Sonu"))
+
+
+n="she is beautiful"
+print(n.find("beautiful"))
+
+
+
+
+a="she is good and she is talented"pos1=a.find("is")
+print(pos1)
+pos2=a.find("is",pos1+1)
+print(pos2)
+
+
+f="sonu"
+print(f.center(8,"_"))
+
+
+
+n=input()
+print(n.center(len(n)+10,"@"))
